b5178f10ce310dfd3cecb41aa65ecf2fac309fdb
Dharitriprava Sahoo <sahoodharitri598@gmail.com>
2026-07-20 17:04:50 +0530

    updated backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const { protect } = require('../middleware/authMiddleware');
-const { admin } = require('../middleware/adminMiddleware');
+const { admin } = require('../middleware/adminMiddleware'); 
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 
